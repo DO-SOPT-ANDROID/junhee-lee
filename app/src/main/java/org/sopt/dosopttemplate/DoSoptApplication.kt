e8ca165a1ee6f83c264d2a60c7003b4fdfd9f251
l2zh <dlwnsgml0109@naver.com>
2023-10-11 18:28:46 +0900

    자동로그인 완료

---
+++ app/src/main/java/org/sopt/dosopttemplate/DoSoptApplication.kt
@@ -0,0 +1,16 @@
+package org.sopt.dosopttemplate
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import org.sopt.dosopttemplate.data.datasource.local.DoSoptStorage
+import timber.log.Timber
+
+class DoSoptApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DoSoptStorage.init(this)
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
--- app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
@@ -2,10 +2,14 @@ package org.sopt.dosopttemplate.presentation
 
 import android.os.Bundle
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.entity.sign.SignInfo
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
+import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.SIGN_INFO
 import sopt.uni.util.binding.BindingActivity
+import sopt.uni.util.extension.parcelable
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -14,8 +18,12 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     private fun initMainPageSetting() {
-        binding.tvNickname.text = intent.getStringExtra("name")
-        binding.tvId.text = intent.getStringExtra("id")
-        binding.tvMbti.text = intent.getStringExtra("mbti")
+        val userInfo = intent.extras?.parcelable<SignInfo>(SIGN_INFO)
+
+        if (userInfo != null) {
+            binding.tvMainpageNickname.text = userInfo.name
+            binding.tvMainpageId.text = userInfo.id
+            binding.tvMainpageMbti.text = userInfo.mbti
+        }
     }
 }
