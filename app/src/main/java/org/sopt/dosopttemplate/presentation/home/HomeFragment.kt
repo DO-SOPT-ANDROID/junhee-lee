39844aef6fc8c3c085ebe4a6a08a5461113e5933
l2zh <dlwnsgml0109@naver.com>
2023-10-25 22:53:19 +0900

    필수과제 완료

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
@@ -1,14 +1,20 @@
-package org.sopt.dosopttemplate.presentation
+package org.sopt.dosopttemplate.presentation.home
 
 import android.os.Bundle
 import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.datasource.local.DummyProfile
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
 import org.sopt.dosopttemplate.util.binding.BindingFragment
 
+@AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-
+    private val homeAdapter by lazy { HomeAdapter() }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val homeProfileList = DummyProfile().mockProfileList
+        binding.rvHome.adapter = homeAdapter
+        homeAdapter.submitList(homeProfileList)
     }
 }
