301dc9ecc094d776affadfd1331d29ea06049f28
l2zh <dlwnsgml0109@naver.com>
2023-11-16 00:33:38 +0900

    Splash Activity 세팅

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/SplashActivity.kt
@@ -0,0 +1,33 @@
+package org.sopt.dosopttemplate.presentation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.ActivitySplashBinding
+import org.sopt.dosopttemplate.presentation.auth.LoginActivity
+import org.sopt.dosopttemplate.util.binding.BindingActivity
+import org.sopt.dosopttemplate.util.extension.startActivity
+
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //1st, make splash activity
+
+        // hide action bar
+        supportActionBar?.hide()
+
+        lifecycleScope.launch{
+            delay(10000)
+            startActivity<LoginActivity>()
+            finish()
+        }
+    }
+}
