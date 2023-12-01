2eda68a1ee44b2d64e358c786c88b4950fae18e2
l2zh <dlwnsgml0109@naver.com>
2023-12-01 16:52:16 +0900

    id,pw 경고 문구 구현, 닉네임 공백시 경고문구 (BidndingAdapter)

--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpActivity.kt
@@ -2,7 +2,10 @@ package org.sopt.dosopttemplate.presentation.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.datasource.local.DoSoptStorage
@@ -10,34 +13,65 @@ import org.sopt.dosopttemplate.databinding.ActivitySignupBinding
 import org.sopt.dosopttemplate.util.UiState
 import org.sopt.dosopttemplate.util.binding.BindingActivity
 import org.sopt.dosopttemplate.util.extension.setOnSingleClickListener
+import org.sopt.dosopttemplate.util.extension.setVisible
 import org.sopt.dosopttemplate.util.extension.showSnackbar
-import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class SignUpActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_signup) {
 
     private val signUpViewModel: SignUpViewModel by viewModels()
-    private lateinit var id: String
-    private lateinit var pw: String
-    private lateinit var name: String
     private lateinit var mbti: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.signupViewModel = signUpViewModel
 
         clickSignUpEnd()
+        observeId()
+        observePw()
+        observeSignUpButton()
+    }
+
+    private fun observeSignUpButton() {
+        signUpViewModel.isEnabledLoginButton.observe(this) {
+            binding.btnSignupEnd.isEnabled = it
+        }
+    }
+
+    private fun observePw() {
+        signUpViewModel.pw.observe(this) {
+            if (signUpViewModel.isValidatePassWord(it)) {
+                binding.tvSignupPasswordErrorMessage.setVisible(INVISIBLE)
+                binding.etSignupPw.backgroundTintList =
+                    ContextCompat.getColorStateList(this, R.color.Gray_500)
+            } else {
+                binding.tvSignupPasswordErrorMessage.setVisible(VISIBLE)
+                binding.etSignupPw.backgroundTintList =
+                    ContextCompat.getColorStateList(this, R.color.Pink_500)
+            }
+        }
+    }
+
+    private fun observeId() {
+        signUpViewModel.id.observe(this) {
+            if (signUpViewModel.isValidateId(it)) {
+                binding.tvSignupIdErrorMessage.setVisible(INVISIBLE)
+                binding.etSignupId.backgroundTintList =
+                    ContextCompat.getColorStateList(this, R.color.Gray_500)
+            } else {
+                binding.tvSignupIdErrorMessage.setVisible(VISIBLE)
+                binding.etSignupId.backgroundTintList =
+                    ContextCompat.getColorStateList(this, R.color.Pink_500)
+            }
+        }
     }
 
     private fun clickSignUpEnd() {
         binding.btnSignupEnd.setOnSingleClickListener {
             getSignUpInfo()
-            if (isValidateForm()) {
-                signUpViewModel.signUp(id, pw, name)
-                observeSignUp()
-            } else {
-                showSnackbar(binding.root, getString(R.string.signup_error_message))
-            }
+            signUpViewModel.signUp()
+            observeSignUp()
         }
     }
 
@@ -62,40 +96,9 @@ class SignUpActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_
 
     private fun getSignUpInfo() {
         with(binding) {
-            id = etSignupId.text.toString()
-            pw = etSignupPw.text.toString()
-            name = etSignupName.text.toString()
             mbti = etSignupMbti.text.toString()
         }
     }
 
-    private fun isValidateForm(): Boolean {
 
-        return isValidateId(id) && isValidatePassWord(pw) && isValidateNickName(name) &&
-                isValidateMbti(mbti)
-    }
-
-    private fun isValidateId(id: String): Boolean {
-        return id.isNotBlank() && ID_REGEX.matcher(id).matches()
-    }
-
-    private fun isValidatePassWord(pw: String): Boolean {
-        return pw.isNotBlank() && PW_REGEX.matcher(pw).matches()
-    }
-
-    private fun isValidateNickName(name: String): Boolean {
-        return name.isNotBlank()
-    }
-
-    private fun isValidateMbti(mbti: String): Boolean {
-        return mbti.isNotBlank()
-    }
-
-    companion object {
-        private const val ID_PATTERN = "(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,10}"
-        private const val PW_PATTERN =
-            "(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#%^&*()])[a-zA-Z0-9!@#%^&*()]{8,12}"
-        val ID_REGEX: Pattern = Pattern.compile(ID_PATTERN)
-        val PW_REGEX: Pattern = Pattern.compile(PW_PATTERN)
-    }
 }
--- app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
@@ -1,20 +1,17 @@
 package org.sopt.dosopttemplate.util.binding
 
-import android.content.res.ColorStateList
 import android.view.View
-import android.widget.EditText
-import androidx.core.content.ContextCompat
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import org.sopt.dosopttemplate.R
 
 object BindingAdapter {
     @JvmStatic
-    @BindingAdapter("setUnderlineColor")
-    fun setUnderlineColor(view: View, isValid: Boolean) {
-        val colorResId = if (isValid) R.color.Pink_500 else R.color.Gray_600
-        if (view is EditText) {
-            view.backgroundTintList =
-                ColorStateList.valueOf(ContextCompat.getColor(view.context, colorResId))
+    @BindingAdapter("setSignupZeroErrorTextVisible")
+    fun setSignupZeroErrorTextVisible(view: TextView, length: Int) {
+        if (length == 0) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.INVISIBLE
         }
     }
 
