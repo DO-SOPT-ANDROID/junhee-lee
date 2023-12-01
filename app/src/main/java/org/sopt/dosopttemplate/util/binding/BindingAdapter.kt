0aa8f59e1f4515b6e2fd4df806682cc6c72d6245
l2zh <dlwnsgml0109@naver.com>
2023-12-01 16:33:03 +0900

    MediatorLiveData 확장함수 추가

--- app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
@@ -1,5 +1,21 @@
 package org.sopt.dosopttemplate.util.binding
 
+import android.content.res.ColorStateList
+import android.view.View
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import org.sopt.dosopttemplate.R
+
 object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("setUnderlineColor")
+    fun setUnderlineColor(view: View, isValid: Boolean) {
+        val colorResId = if (isValid) R.color.Pink_500 else R.color.Gray_600
+        if (view is EditText) {
+            view.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(view.context, colorResId))
+        }
+    }
 
 }
